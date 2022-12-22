fb09b714d319e063371f5890b60ffcc16d40101d
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-12-22 17:08:44 +0530

    weather app format

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { useState, useEffect} from 'react';
+import  axios  from 'axios';
 import './App.css';
 
+import ImgGoogleMap from "./google-maps.png"
+
+
 function App() {
+
+  const [city, setCity]= useState("Nagpur");
+  const [temp, setTemp]= useState("0");
+  const [description, setDescription]= useState("");
+
+  useEffect(()=>{
+    async function loadData(){
+      const response = await axios.get('')
+      if(response.status === 200){
+        const temp = response.data.main.temp - 273.15
+        setTemp(Math.round(temp));  
+      }
+    }
+    loadData();
+
+  }, [city])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='app-title'>Weather App</h1>
+      <div className='search-container'> 
+        <img src={ImgGoogleMap} className="img-location"/>
+        <input type="text" placeholder='Enter Your City'
+         className='input-city'
+         value={city}
+         onChange={(e)=>setCity(e.target.value)} />
+      </div>
+
+      <h2>City:{city}</h2>
+      <h2>Temp:{temp}</h2>
+    </>
   );
 }
 
